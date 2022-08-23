58deb6898109d19c3a4f84bed6e9fcd51613f2ca
Azul Siret Cordero <101304946+Liar17@users.noreply.github.com>
2022-08-23 14:51:35 -0600

    Add files via upload

---
+++ Unidad 1/estDat_u1_1/src/estdat_u1_1/EstDat_u1_1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package estdat_u1_1;
+import java.util.Arrays;
+import java.util.Scanner;
+/**
+ *
+ * @author invitado
+ */
+public class EstDat_u1_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[] nombre = new String[5];
+        
+        nombre[0] = "Luis";
+        nombre[1] = "Maria";
+        nombre[2] = "Carlos";
+        nombre[3] = "Jose";
+        nombre[4] = "Ismael";
+        
+        System.out.println(Arrays.toString(nombre));
+        
+        
+     
+    }
+    
+}

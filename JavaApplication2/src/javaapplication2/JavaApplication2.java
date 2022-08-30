c87921866a936320448029ded2ad52608916dba1
Azul Siret Cordero <101304946+Liar17@users.noreply.github.com>
2022-08-30 14:45:53 -0600

    Add files via upload

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication2;
+
+/**
+ *
+ * @author invitado
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 20, b=10;
+        String x ="Programacion", y="objetos";
+        
+        int ar[]={1, 2, 3};
+        int br[]={1, 2, 3};
+        
+        boolean condition=true;
+        
+        //uso de operadores condicionales
+        System.out.println("a==b: "+(a==b));
+        System.out.println("a<b: "+(a<b));
+        System.out.println("a<=b: "+(a<=b));
+        System.out.println("a>b: "+(a>b));
+        System.out.println("a>=b: "+(a>=b));
+        System.out.println("a!=b: "+(a!=b));
+        
+        System.out.println("x==y: "+(ar==br));
+        System.out.println("condition==true: "+(condition==true));
+        
+        //¿los arreglos se pueden comparar con operadores relacionales?
+        //si o no y ¿por que?
+    }
+    
+}
